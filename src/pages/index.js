1bedd7e30f5b584a1b5a15f213930c6487d3ea8f
katarem <crismigesp@gmail.com>
2025-05-28 20:42:23 +0100

    fixing broken liknks

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,11 +17,6 @@ function HomepageHeader() {
         </Heading>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
-          </Link>
         </div>
       </div>
     </header>
@@ -36,7 +31,6 @@ export default function Home() {
       description="Description will go into a meta tag in <head />">
       <HomepageHeader />
       <main>
-        <HomepageFeatures />
       </main>
     </Layout>
   );
